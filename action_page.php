7acdd1a329c37963be38b3251f44354dbed732cf
byhaqie31 <byhaqie1455@gmail.com>
2021-04-20 13:04:14 +0800

    mythbuster 2.0

---
+++ action_page.php
@@ -0,0 +1,85 @@
+<html><head>
+    <meta charset="utf-8">
+<title>Forms action page</title>
+<link rel="stylesheet" href="/w3css/4/w3.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>MISCONCEPTION ON VACCINE</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://m.w3newbie.com/you-tube.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+</head><body class="w3-container" data-new-gr-c-s-check-loaded="14.1006.0" data-gr-ext-installed="">
+
+<!-- Navigation  Panel -->
+<nav class="navbar navbar-expand-md navbar-light bg-light ">
+<div class="container-fluid">
+	<a class="navbar-brand" href="index.html"><img src="img/Logo MB.png"></a>
+	<a class="navbar-brand" href="index.html">MYTH BUSTER.COM</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" 
+	data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="index.html">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="introduction.html">Introduction</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="misconception.html">Misconception</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="covid.html">Miscellaneous</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="ourteam.php">Our Team</a>
+			</li>
+		</ul>	
+	</div>
+
+
+</div>
+</nav> 
+
+<br>
+<br>
+<br>
+<br>
+<!-- Main Page -->
+<div class="carousel-inner">
+	<div class="carousel-item active">
+		<img src="img/Circle.jpg">
+		<div class="carousel-caption">
+				<h1>Your feedback was sent</h1>
+<h2>Thank you, <?php echo $_GET["Name"]?></h2>
+<br>
+<?php echo $_GET["Email"]?>
+<br>
+		</div>
+	</div>
+
+</div>
+</div>
+
+
+<footer>
+<div class="container-fluid padding">
+<div class="row text-center">
+	
+	<div class="col-12">
+				<h5>&copy; Myth Buster.com</h5>
+	</div>
+</div>
+</div>
+</footer>
+
+</body></html>
+
+
---
+++ ourteam.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>MISCONCEPTION ON VACCINE</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://m.w3newbie.com/you-tube.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+</head>
+<body>
+
+<!-- Navigation  Panel -->
+<nav class="navbar navbar-expand-md navbar-light bg-light ">
+<div class="container-fluid">
+	<a class="navbar-brand" href="index.html"><img src="img/Logo MB.png"></a>
+	<a class="navbar-brand" href="index.html">MYTH BUSTER.COM</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" 
+	data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item ">
+				<a class="nav-link" href="index.html">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="introduction.html">Introduction</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="misconception.html">Misconception</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="covid.html">Miscellaneous</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="ourteam.php">Our Team</a>
+			</li>
+		</ul>	
+	</div>
+
+
+</div>
+</nav> 
+
+<!-- Main Page -->
+<div class="carousel-inner">
+	<div class="carousel-item active">
+		<img src="img/IntroPic.jpg">
+		<div class="carousel-caption">
+			<h1 class="display-2">MISCONCEPTION OF VACCINE</h1>
+			<br>
+			<br>
+			<div class="w3-light-grey w3-padding-large w3-padding-32 w3-margin-top" id="contact">
+    <h3 class="w3-center">Contact Us</h3>
+    <hr>
+    <h4 class="w3-center">Any comments or feedbacks, kindly fill in the form:</h4>
+
+    <form action="action_page.php" target="_blank">
+      <div class="w3-section">
+        <label>Name:</label><br>
+        <input class="w3-input w3-border" type="text" required="" name="Name">
+      </div><br>
+      <div class="w3-section">
+        <label>Email:</label><br>
+        <input class="w3-input w3-border" type="text" required="" name="Email">
+      </div><br>
+      <div class="w3-section">
+        <label>Message:</label>
+		<br>
+        <input class="w3-input w3-border" required="" name="Message">
+      </div><br>
+      <button type="submit" class="w3-button w3-block w3-dark-grey">Send</button>
+    </form>
+  </div>
+		</div>
+	</div>
+
+</div>
+
+
+<!-- Footer -->
+<footer>
+
+<div class="container-fluid padding">
+<div class="row text-center">
+		<div class="col-sm-12 col-md-6 col-lg-3" data-aos="fade-up">
+			<img class="icon" src="img/nobita.png">
+			<br>
+			<br>
+			<p class="action">Arief bin shamsuddin</p>
+			<p class="action">17083141/2</p>
+		</div>
+		<div class="col-sm-12 col-md-6 col-lg-3" data-aos="fade-up">
+			<img class="icon" src="img/giant.png">
+			<br>
+			<br>
+			<p class="action">Nik Muhammad Sofi Danial bin Aziz</p>
+			<p class="action">17137560/2</p>
+		</div>
+		<div class="col-sm-12 col-md-6 col-lg-3" data-aos="fade-up">
+			<img class="icon" src="img/suneo.png">
+			<br>
+			<br>
+			<p class="action">Muqri Hafiy bin Mazli</p>
+			<p class="action">17081180/2</p>
+		</div>
+		<div class="col-sm-12 col-md-6 col-lg-3" data-aos="fade-up">
+			<img class="icon" src="img/doraemon.png"><br>
+			<br>
+			<p class="action">Ahmad Baihaqie bin Mohd Yusri</p>
+			<p>17064554/2</p>
+		</div>
+	</div>
+<div class="row text-center">
+	
+	<div class="col-12">
+				<h5>&copy; Myth Buster.com</h5>
+	</div>
+</div>
+</div>
+</footer>
+
+
+
+</body>
+</html>
